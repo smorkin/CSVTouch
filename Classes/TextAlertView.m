a696d952d6c2eedacd750123dd70fe4a08a51259
simon <simon>
2011-01-10 00:01:40 +0000

    Code polish

--- Classes/TextAlertView.m
+++ Classes/TextAlertView.m
@@ -42,48 +42,6 @@
 			  otherButtonTitles:otherButtonTitles, nil];
 	if (self)
 	{
-		
-		
-		
-//		UITextField *textField;
-//		UITextField *textField2;
-//		
-//		UIAlertView *prompt = [[UIAlertView alloc] initWithTitle:@"Username and password" 
-//														 message:@"\n\n\n" // IMPORTANT
-//														delegate:nil 
-//											   cancelButtonTitle:@"Cancel" 
-//											   otherButtonTitles:@"Enter", nil];
-//		
-//		textField = [[UITextField alloc] initWithFrame:CGRectMake(12.0, 50.0, 260.0, 25.0)]; 
-//		[textField setBackgroundColor:[UIColor whiteColor]];
-//		[textField setPlaceholder:@"username"];
-//		[prompt addSubview:textField];
-//		
-//		textField2 = [[UITextField alloc] initWithFrame:CGRectMake(12.0, 85.0, 260.0, 25.0)]; 
-//		[textField2 setBackgroundColor:[UIColor whiteColor]];
-//		[textField2 setPlaceholder:@"password"];
-//		[textField2 setSecureTextEntry:YES];
-//		[prompt addSubview:textField2];
-//		
-//		// set place
-//		[prompt setTransform:CGAffineTransformMakeTranslation(0.0, 110.0)];
-//		[prompt show];
-//		[prompt release];
-//		
-//		// set cursor and show keyboard
-//		[textField becomeFirstResponder];
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		// Create and add UITextField to UIAlertView
 		UITextField *myTextField = [[[UITextField alloc] initWithFrame:CGRectZero] retain];
 		myTextField.autocorrectionType = UITextAutocorrectionTypeNo;
@@ -102,12 +60,6 @@
 - (void) show {
 	[super show];
 	[[self textField] performSelector:@selector(becomeFirstResponder) withObject:nil afterDelay:0.6];
-//	[[self textField] becomeFirstResponder];
-}
-
-- (UIView *) buttonsView
-{
-	return [[self subviews] lastObject]; // TODO: Kan bara anropas innan layoutDone=YES
 }
 
 /*
@@ -115,16 +67,10 @@
  */
 - (void)layoutSubviews {
 	[super layoutSubviews];
-	CGRect frame = [self frame];
-	CGFloat alertWidth = frame.size.width;
 	
 	// Perform layout of subviews just once
 	if(!layoutDone) {
 		CGRect viewFrame;
-		
-//		viewFrame = [[self buttonsView] frame];
-//		viewFrame.origin.y += kUITextFieldHeight;
-//		[[self buttonsView] setFrame:viewFrame];
 		
 		[self addSubview:self.textField];
 		viewFrame = CGRectMake(kUITextFieldXPadding, 
@@ -132,23 +78,12 @@
 							   self.frame.size.width - 4.0*kUITextFieldXPadding, 
 							   kUITextFieldHeight);
 		[self.textField setFrame:viewFrame];
-		
-		// size UIAlertView frame by height of UITextField
+
+		// No longer necessary since we instead at string @"\n\n" as message in alert
+//      size UIAlertView frame by height of UITextField
 //		frame.size.height += kUITextFieldHeight + 2.0;
 //		[self setFrame:frame];
 		layoutDone = YES;
-	}
-	else
-	{
-		// reduce the x placement and width of the UITextField based on UIAlertView width
-		for(UIView *view in self.subviews){
-		    if([view isKindOfClass:[UITextField class]]){
-				CGRect viewFrame = [view frame];
-				viewFrame.origin.x = kUITextFieldXPadding;
-				viewFrame.size.width = alertWidth - 4.0*kUITextFieldXPadding;
-				[view setFrame:viewFrame];
-		    }
-		}
 	}
 }
 
